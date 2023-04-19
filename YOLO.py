a94c20eed03ef95e90f9fb3060fbf71f804d3460
maligir <maligir@utexas.edu>
2023-04-18 20:16:18 -0500

    made YOLO.py print class with highest confidence

--- YOLO.py
+++ YOLO.py
@@ -8,10 +8,18 @@ model = YOLO("yolov8x-cls.pt")
 # results = model.predict(source="folder", show=True) # Display preds. Accepts all YOLO predict arguments
 
 # from PIL
-im1 = Image.open("/home/bwilab/Documents/glassBottle.png")
-results = model.predict(source=im1, save=True)  # save plotted images
+im1 = Image.open("/home/bwilab/Wyatt_Aman_Sahir_John_FRI1/FRI_One_Recycle_Detection/testImages/plasticBag.png")
+results = model.predict(source=im1, save=False)  # save plotted images
 
 
 # from list of PIL/ndarray
 results = model.predict(source=im1)
-print(results[0])
+
+# Find the index with the highest probability
+best_class_idx = results[0].probs.argmax()
+
+# Get the class name with the highest probability
+class_names = list(results[0].names.values())
+best_class_name = class_names[best_class_idx]
+
+print(best_class_name)
